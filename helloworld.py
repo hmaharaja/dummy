9cdc19542641fdc5c84838d3d165257c6b487bf9
Hiranya Maharaja <h.maharaja@mail.utoronto.ca>
2021-09-17 13:50:56 -0400

    c1

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,8 @@
 if __name__ == "__main__":
     print("Been at uoft for 4 years")
     print("collaborator name for activity 4: Michael Zheng")
+<<<<<<< HEAD
     print("rebase commit 4")
+=======
+    print("rebase commit 1")
+>>>>>>> 3cae1b7 (c1)
